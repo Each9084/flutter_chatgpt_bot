36bc68162313de0f5f3bf22a46c93da38e3a452d
Each9084 <each9084@gmail.com>
2024-11-09 20:04:22 +0000

    complete main function

--- lib/screen/chat_bot.dart
+++ lib/screen/chat_bot.dart
@@ -20,21 +20,66 @@ class _ChatBotState extends State<ChatBotScreen> {
   StreamSubscription? subscription;
 
   //api key
-  final String apikey = '';
+  final String apikey = kApiKey;
 
   ///List message 换为不可修改的列表
-  final List<MessageModel> message =
-      List.unmodifiable([MessageModel(true, 'hi')]);
+  /*final List<MessageModel> messageList =
+      List.unmodifiable([MessageModel(true, 'hi')]);*/
+  final messageList = [MessageModel(true, 'Hi')];
+
+  void sendMessage(String message) async {
+    final request =
+        ChatCompleteText(model: Gpt4oMiniChatModel(), maxToken: 400, messages: [
+      Map.of({'role': 'user', 'content': message})
+    ]);
+
+
+    /*
+    subscription = openAI.onChatCompletion(request: request).asStream().listen((it){
+      setState(() {
+        messageList.add(MessageModel(true, "${it?.choices.last.message}"));
+      });
+    });*/
+
+    ///流式响应,异步处理,实时更新
+   /* openAI.onChatCompletionSSE(request: request).listen((it) {
+      setState(() {
+        messageList.add(MessageModel(true, it.choices?.last.message?.content.toString()??'Error'));
+      });
+    });*/
+
+
+    ///一次性响应
+    final response = await openAI.onChatCompletion(request: request);
+
+    setState(() {
+      //TODO 可能有问题
+      messageList.add(MessageModel(
+          true, response?.choices.last.message?.content.toString() ?? 'Error'));
+    });
+
+    //TODO 流式响应
+    /* openAI.onChatCompletionSSE(request: request).listen((it) {
+      debugPrint(it.choices?.last.message?.content);
+    });*/
+
+    /* for (var message in messageList) {
+      print(message.message); // 打印每个消息的内容
+      print('高度是:'+MediaQuery.of(context).size.height.toString());
+    }*/
+
+  }
 
   @override
   void initState() {
-    openAI = OpenAI.instance.build(token: kToken);
+    openAI = OpenAI.instance.build(token: kApiKey);
     super.initState();
   }
 
   @override
   void dispose() {
     // TODO: implement dispose
+    subscription?.cancel();
     super.dispose();
   }
 
@@ -50,24 +95,80 @@ class _ChatBotState extends State<ChatBotScreen> {
               /**
                * List View 设置对话 一来一回
                * */
-              Expanded(
-                  flex: 7,
-                  child: ListView.builder(
-                    ///TODO: Find out the builder and itemCount
-                    itemCount: message.length,
-                    //苹果的滚动风格 到底部会回弹
-                    physics: BouncingScrollPhysics(),
-                    itemBuilder: (context,index) {
-                      return _botCard(index:index);
-                    },
-                  ))
+              _messageList(),
+              /*SizedBox(height: 75,)*/
             ],
           ),
 
-          /*
-          * 底部导航
-          * */
+          /**
+           * 底部导航
+           * */
           _bottomNavigation(context)
+        ],
+      ),
+    );
+  }
+
+  Expanded _messageList() {
+    return Expanded(
+        flex: 7,
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 75),
+          child: ListView.builder(
+            ///TODO: Find out the builder and itemCount
+            itemCount: messageList.length,
+            //苹果的滚动风格 到底部会回弹
+            physics: BouncingScrollPhysics(),
+            itemBuilder: (context, index) {
+              return messageList[index].isBot
+                  ? _botCard(index: index)
+                  : _userCard(index: index);
+            },
+          ),
+        ));
+  }
+
+  Padding _userCard({required int index}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 5),
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.centerRight,
+            child: CircleAvatar(
+                backgroundColor: Colors.teal,
+                child: Icon(
+                  Icons.person,
+                  color: Colors.black,
+                )),
+          ),
+          Positioned(
+            right: 50,
+            child: Column(
+              children: [
+                Container(
+                  //TODO padding应该在外面 不能写在里面
+                  padding: EdgeInsets.symmetric(horizontal: 5, vertical: 0),
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius:
+                          BorderRadius.only(bottomRight: Radius.circular(5)),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black.withOpacity(.23),
+                            offset: Offset(3, .2),
+                            //阴影模糊半径
+                            blurRadius: kDefault)
+                      ]),
+                  child: Text(
+                    //第二个是model的字段
+                    messageList[index].message,
+                  ),
+                ),
+
+              ],
+            ),
+          )
         ],
       ),
     );
@@ -76,43 +177,37 @@ class _ChatBotState extends State<ChatBotScreen> {
   //TODO 详细了解命名参数和其他的区别
   Padding _botCard({required int index}) {
     return Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 5),
-                      child: Stack(
-                        children: [
-                          CircleAvatar(
-                            child: Icon(
-                              Icons.person,
-                              size: 30,
-                            ),
-                          ),
-                          Positioned(
-                            left: 50,
-                            child: Column(
-                              children: [
-                                Container(
-                                  //TODO padding应该在外面 不能写在里面
-                                  padding:EdgeInsets.symmetric(horizontal: 5,vertical: 0),
-                                  decoration: BoxDecoration(
-                                    color: Colors.blue,
-                                    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(5)),
-                                      boxShadow: [
-                                        BoxShadow(
-                                            color: Colors.black.withOpacity(.23),
-                                            offset: Offset(3, .2),
-                                            //阴影模糊半径
-                                            blurRadius: kDefault)
-                                      ]
-                                  ),
-                                  child: Text(
-                                    'data',
-                                  ),
-                                )
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    );
+      padding: const EdgeInsets.symmetric(horizontal: 5,vertical: 5),
+      child: Stack(
+        children: [
+          CircleAvatar(
+            backgroundImage: NetworkImage(
+                kAsukaAvatar),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 60,),
+            //TODO padding应该在外面 不能写在里面
+            padding: EdgeInsets.symmetric(horizontal: 5, vertical: 0),
+            decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius:
+                    BorderRadius.only(bottomLeft: Radius.circular(5)),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black.withOpacity(.23),
+                      offset: Offset(3, .2),
+                      //阴影模糊半径
+                      blurRadius: kDefault)
+                ]),
+            child: Text(
+              messageList[index].message,
+              //自动换行
+              softWrap: true,
+            ),
+          )
+        ],
+      ),
+    );
   }
 
   Align _bottomNavigation(BuildContext context) {
@@ -143,12 +238,23 @@ class _ChatBotState extends State<ChatBotScreen> {
             controller: txtMessage,
             decoration: InputDecoration(
                 hintStyle: TextStyle(fontSize: 25),
-                suffixIcon: Icon(
-                  Icons.send,
-                  size: 30,
-                  color: Colors.blue,
+                suffixIcon: GestureDetector(
+                  onTap: () {
+                    FocusScope.of(context).unfocus();
+                    setState(() {
+                      sendMessage(txtMessage.text.toString());
+                      messageList
+                          .add(MessageModel(false, txtMessage.text.toString()));
+                    });
+                    txtMessage.clear();
+                  },
+                  child: Icon(
+                    Icons.send,
+                    size: 30,
+                    color: Colors.blue,
+                  ),
                 ),
-                hintText: '  plz input the question',
+                hintText: '  plz input the question here',
                 disabledBorder: InputBorder.none,
                 enabledBorder: InputBorder.none),
           ),
@@ -179,7 +285,7 @@ class _ChatBotState extends State<ChatBotScreen> {
           children: [
             Icon(
               Icons.arrow_back_rounded,
-              color: Colors.black,
+              color: Colors.white,
               size: kDefault * 10,
             ),
             Padding(
@@ -188,7 +294,7 @@ class _ChatBotState extends State<ChatBotScreen> {
               ),
               child: CircleAvatar(
                 backgroundImage: NetworkImage(
-                    'https://img0.baidu.com/it/u=1941938688,2932034800&fm=253&fmt=auto&app=120&f=JPEG?w=801&h=500'),
+                    kAsukaAvatar),
               ),
             ),
             Column(
